e62ed6f29e7da0c13937b293e35d579393d17717
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-07-25 12:24:07 +0530

    new files added

--- models/user.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password_hash: { type: String, required: true },
-});
-
-module.exports = mongoose.model('user', UserSchema);
