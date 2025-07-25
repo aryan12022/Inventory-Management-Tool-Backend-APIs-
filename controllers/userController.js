dafda6c2b712a672a1b443b704cf5b7f38ce93e9
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-07-25 12:29:55 +0530

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,60 @@
+const User = require('../models/UserModel');
+const jwt = require('jsonwebtoken');
+
+// @desc    Register a new user
+// @route   POST /register
+// @access  Public
+const registerUser = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userExists = await User.findOne({ username });
+
+        if (userExists) {
+            return res.status(409).json({ message: 'User already exists' });
+        }
+
+        const user = await User.create({
+            username,
+            password,
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                username: user.username,
+            });
+        } else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// @desc    Auth user & get token
+// @route   POST /login
+// @access  Public
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (user && (await user.matchPassword(password))) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: '1h',
+            });
+            res.json({
+                message: "Login successful",
+                access_token: token,
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid username or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser };
